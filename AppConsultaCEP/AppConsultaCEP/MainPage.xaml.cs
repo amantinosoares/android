bcd2fbb8f6124f4658ce917fe0993b44c2941b56
ponto sem ponto <amantino.fsoares@outlook.com>
2019-07-24 21:33:20 -0300

    AppCEP

---
+++ AppConsultaCEP/AppConsultaCEP/MainPage.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using AppConsultaCEP.Servico.Modelo;
+using AppConsultaCEP.Servico;
+
+namespace AppConsultaCEP
+{
+    
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            BOTAO.Clicked += BuscarCEP;
+        }
+
+        private void BuscarCEP(object sender, EventArgs e)
+        {
+            if (validaCEP(TXTCEP.Text.Trim()))
+            {
+                Endereco end = ViaCEPServico.BuscarEnderecoViaCEP(TXTCEP.Text.Trim());
+
+                RESULTADO.Text = string.Format("Endereço: {0},{1} {2}", end.Localidade, end.Uf, end.Logradouro);
+            }
+
+          
+           
+        }
+
+        private bool validaCEP(string cep)
+        {
+            bool valido = true;
+
+            if(cep.Length != 8)
+            {
+                DisplayAlert("ERRO", "CEP inválido", "OK");
+
+                valido = false;
+            }
+
+            return valido;
+        }
+
+    }
+}
---
+++ AppConsultaCEP/AppConsultaCEP/Servico/ViaCEPServico.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net;
+using AppConsultaCEP.Servico.Modelo;
+using Newtonsoft.Json;
+
+namespace AppConsultaCEP.Servico
+{
+    public class ViaCEPServico
+    {
+        private static string EnderecoURL = "http://viacep.com.br/ws/{0}/json/";
+
+        public static Endereco BuscarEnderecoViaCEP(string cep)
+        {
+            string NovoEnderecoURL = string.Format(EnderecoURL, cep);
+
+            WebClient wc = new WebClient();
+
+            string Conteudo = wc.DownloadString(NovoEnderecoURL);
+
+            Endereco End = JsonConvert.DeserializeObject<Endereco>(Conteudo);
+
+            return End;
+
+
+
+        }
+    }
+}
